ecf2b65327fd1b758026e9e33f67f884e690f264
phoon <marcus60.2001@gmail.com>
2024-11-28 19:39:44 +0300

    Initial commit

---
+++ src/main/java/com/dubrovsky/task/restful/TaskRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.dubrovsky.task.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dubrovsky/task/restful/TaskRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubrovsky.task.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
